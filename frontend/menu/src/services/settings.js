7a4f7a186e573f4ec0cf6e90abe9a20db7a7e22b
adolfoarmas <adolfoarmas.90@gmail.com>
2023-04-16 12:44:33 -0300

    frontend service settings

--- frontend/menu/src/services/settings.js
+++ frontend/menu/src/services/settings.js
@@ -1,11 +1,11 @@
-export const DISHES_END_POINT = `${process.env.REACT_APP_API_URL}/api/dishes/`
+export const DISHES_END_POINT = `http://api:8000/api/dishes/`
 
-export const DISH_CATEGORIES_END_POINT = `${process.env.REACT_APP_API_URL}/api/dishCategory/`
+export const DISH_CATEGORIES_END_POINT = `http://api:8000/api/dishCategory/`
 
-export const DISH_CATEGORIES = `${process.env.REACT_APP_API_URL}/api/dishCategory/`
+export const DISH_CATEGORIES = `http://api:8000/api/dishCategory/`
 
-export const USER_LOGIN_END_POINT = `${process.env.REACT_APP_API_URL}/api/auth/login/`
+export const USER_LOGIN_END_POINT = `http://api:8000/api/auth/login/`
 
-export const USER_LOGOUT_END_POINT = `${process.env.REACT_APP_API_URL}/api/auth/logout/`
+export const USER_LOGOUT_END_POINT = `http://api:8000/api/auth/logout/`
 
-export const USERS_END_POINT = `${process.env.REACT_APP_API_URL}/api/users/`
+export const USERS_END_POINT = `http://api:8000/api/users/`
