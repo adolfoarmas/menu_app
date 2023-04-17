ff0a137ee688a727da22ad45eedfa3d57d6828c6
adolfoarmas <adolfoarmas.90@gmail.com>
2023-04-17 16:39:27 -0300

    frontend api endpoint change

--- frontend/menu/src/services/settings.js
+++ frontend/menu/src/services/settings.js
@@ -1,11 +1,11 @@
-export const DISHES_END_POINT = `http://api/api/dishes/`
+export const DISHES_END_POINT = `http://10.88.0.18:8000/api/dishes/`
 
-export const DISH_CATEGORIES_END_POINT = `http://api/api/dishCategory/`
+export const DISH_CATEGORIES_END_POINT = `http://10.88.0.18:8000/api/dishCategory/`
 
-export const DISH_CATEGORIES = `http://api/api/dishCategory/`
+export const DISH_CATEGORIES = `http://10.88.0.18:8000/api/dishCategory/`
 
-export const USER_LOGIN_END_POINT = `http://api/api/auth/login/`
+export const USER_LOGIN_END_POINT = `http://10.88.0.18:8000/api/auth/login/`
 
-export const USER_LOGOUT_END_POINT = `http://api/api/auth/logout/`
+export const USER_LOGOUT_END_POINT = `http://10.88.0.18:8000/api/auth/logout/`
 
-export const USERS_END_POINT = `http://api/api/users/`
+export const USERS_END_POINT = `http://10.88.0.18:8000/api/users/`
